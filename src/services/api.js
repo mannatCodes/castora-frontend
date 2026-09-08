8fe160fac267e12b8932acd57f699a8aa6cf814f
laptopmannat2346 <tharejamannat1@gmail.com>
2026-09-08 12:42:04 +0530

    frontend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
+const API_BASE_URL =
+   process.env.REACT_APP_API_BASE_URL || 'https://castora-backend-1.onrender.com';
 
 const api = axios.create({
    baseURL: API_BASE_URL,
